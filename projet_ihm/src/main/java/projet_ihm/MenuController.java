9cfb3e9250407e37f64ba08b07afef27f2cde5b9
Zuck <julienlesne17@gmail.com>
2024-01-29 15:39:45 +0100

    Added quit

--- projet_ihm/src/main/java/projet_ihm/MenuController.java
+++ projet_ihm/src/main/java/projet_ihm/MenuController.java
@@ -1,12 +1,23 @@
 package projet_ihm;
 
 import java.io.IOException;
+
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class MenuController {
 
     @FXML
+    Button quit;
+
+    @FXML
     private void switchToEditor() throws IOException {
         App.setRoot("editor");
+    }
+
+    @FXML
+    public void initialize() {
+        quit.setOnMouseClicked(event -> Platform.exit());
     }
 }
