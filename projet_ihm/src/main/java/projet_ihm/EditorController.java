7c908deab7191e97fd53a620c4d7448ab6062b06
Lilian <liliannalil347@gmail.com>
2024-01-29 14:55:53 +0100

    commit to Julien

--- projet_ihm/src/main/java/projet_ihm/EditorController.java
+++ projet_ihm/src/main/java/projet_ihm/EditorController.java
@@ -1,9 +1,17 @@
 package projet_ihm;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 
 public class EditorController {
+
+    @FXML
+    GridPane gridpane;
 
     @FXML
     private void switchToMenu() throws IOException {
@@ -12,6 +20,22 @@ public class EditorController {
 
     @FXML
     private void createGridPane() {
+        // Image image = new Image(App.class.getResource("floor.png").toExternalForm());
+        // ImageView view = new ImageView(image);
+        // view.setPreserveRatio(true);
+        int size = 10;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                Button button = new Button();
+                button.setText("Button" + i + j);
+                // button.setGraphic(view);
+                gridpane.add(button, i, j);
+            }
+        }
+    }
 
+    @FXML
+    public void initialize() {
+        createGridPane();
     }
 }
