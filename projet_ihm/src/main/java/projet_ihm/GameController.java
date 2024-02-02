7e0909db7c99288c50e6eb5c1d18b604948e4a79
Zuck <julienlesne17@gmail.com>
2024-02-02 19:45:07 +0100

    working test for the game

--- projet_ihm/src/main/java/projet_ihm/GameController.java
+++ projet_ihm/src/main/java/projet_ihm/GameController.java
@@ -1,12 +1,45 @@
 package projet_ihm;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 
 public class GameController {
 
-     @FXML
+    @FXML
+    BorderPane root;
+
+    @FXML
     public void initialize() {
-        
+        this.loadGame();
+    }
+
+    private void loadGame() {
+
+        GridPane gridPane = new GridPane();
+        String style = "-fx-border-color: black; -fx-border-width: 1;";
+        gridPane.setStyle(style);
+
+        String path = "/images/floor.png";
+        Image floor = new Image(getClass().getResource(path).toExternalForm());
+        int gridSize = 10;
+
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                ImageView background = new ImageView(floor);
+                StackPane container = new StackPane(background);
+                container.setStyle(style);
+                container.setPadding(new javafx.geometry.Insets(0));
+                gridPane.add(container, i, j);
+            }
+        }
+
+        // Ajout de la grille au centre
+        root.setCenter(gridPane);
     }
     
 }
