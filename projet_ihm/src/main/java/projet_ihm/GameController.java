990ebe6807aebe201d684c0a55a4cc4d5dcd9396
Zuck <julienlesne17@gmail.com>
2024-02-03 20:30:05 +0100

    Added menu in Game

--- projet_ihm/src/main/java/projet_ihm/GameController.java
+++ projet_ihm/src/main/java/projet_ihm/GameController.java
@@ -1,11 +1,18 @@
 package projet_ihm;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
 public class GameController {
@@ -14,32 +21,76 @@ public class GameController {
     BorderPane root;
 
     @FXML
+    GridPane gameGrid;
+
+    @FXML
     public void initialize() {
+        root.addEventFilter(KeyEvent.KEY_PRESSED, event -> {   
+            if (event.getCode() == KeyCode.ESCAPE) {
+                this.showPausePopup();
+            }
+        });
         this.loadGame();
     }
 
+    private void switchToMenu() throws IOException {
+        App.setRoot("menu");
+    }
+
     private void loadGame() {
-
-        GridPane gridPane = new GridPane();
         String style = "-fx-border-color: black; -fx-border-width: 1;";
-        gridPane.setStyle(style);
-
-        String path = "/images/floor.png";
-        Image floor = new Image(getClass().getResource(path).toExternalForm());
+        gameGrid.setStyle(style);
+        gameGrid.setHgap(1);
+        gameGrid.setVgap(1);
+        String floorPath = "/images/floor.png";
+        Image floor = new Image(getClass().getResource(floorPath).toExternalForm());
         int gridSize = 10;
 
+        // Ajoutez les ImageView pour représenter le fond de la carte ici
         for (int i = 0; i < gridSize; i++) {
             for (int j = 0; j < gridSize; j++) {
-                ImageView background = new ImageView(floor);
-                StackPane container = new StackPane(background);
+                ImageView cell = new ImageView(floor);
+                cell.setFitWidth(50);
+                cell.setFitHeight(50);
+                StackPane container = new StackPane(cell);
+                container.getStyleClass().add("character");
                 container.setStyle(style);
-                container.setPadding(new javafx.geometry.Insets(0));
-                gridPane.add(container, i, j);
+                gameGrid.add(container, i, j);
             }
         }
-
-        // Ajout de la grille au centre
-        root.setCenter(gridPane);
     }
     
+    private void showPausePopup() {
+        // Créer une fenêtre pop-up avec un titre
+        Alert pauseAlert = new Alert(AlertType.CONFIRMATION);
+        pauseAlert.setTitle("Pause");
+        pauseAlert.setHeaderText("Need a break ?");
+
+        // Ajouter des boutons personnalisés avec des actions associées
+        ButtonType muteButton = new ButtonType("Mute Sound");
+        ButtonType backButton = new ButtonType("Back");
+        ButtonType mainMenuButton = new ButtonType("Main Menu");
+        pauseAlert.getButtonTypes().setAll(muteButton, backButton, mainMenuButton);
+
+        // Afficher la fenêtre pop-up
+        pauseAlert.showAndWait().ifPresent(buttonType -> {
+            // Gérer les actions en fonction du bouton cliqué
+            if (buttonType == muteButton) {
+                // Action pour le bouton "Mute Sound"
+                System.out.println("MUTE");
+            } else if (buttonType == backButton) {
+                // Action pour le bouton "Back"
+                System.out.println("Back");
+            } else if (buttonType == mainMenuButton) {
+                // Action pour le bouton "Main Menu"
+                try {
+                    this.switchToMenu();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
 }
